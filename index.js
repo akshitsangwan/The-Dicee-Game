1ef126a3d4ff6b2487c494bb92da9a5e1d60d8b5
Akshit Sangwan <85335888+akshitsangwan@users.noreply.github.com>
2022-10-04 14:54:15 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,14 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomImage1 = "images/dice" + randomNumber1 + ".png";
+let randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomImage1);
+document.querySelector(".img2").setAttribute("src", randomImage2);
+
+if (randomNumber1 == randomNumber2)
+    document.querySelector(".container h1").innerHTML = "👏Draw👏";
+else if (randomNumber1 > randomNumber2)
+    document.querySelector(".container h1").innerHTML = "🥳Player 1 Wins🥳";
+else
+    document.querySelector(".container h1").innerHTML = "🎊Player 2 Wins🎊";
